42895040a51fd3532c28e6dea128d6d546445288
Rafael Moura <rafael.g.moura@hotmail.com>
2023-01-16 19:43:52 -0300

    feat: firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'atividade3-141cc',
+    appId: '1:713147221696:web:edd1b5726f0a5ad57c4f25',
+    storageBucket: 'atividade3-141cc.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAigfIPxFMa5B_AQGtBEx0KuWmTEgl6LDA',
+    authDomain: 'atividade3-141cc.firebaseapp.com',
+    messagingSenderId: '713147221696',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'atividade3-141cc',
+    appId: '1:713147221696:web:edd1b5726f0a5ad57c4f25',
+    storageBucket: 'atividade3-141cc.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAigfIPxFMa5B_AQGtBEx0KuWmTEgl6LDA',
+    authDomain: 'atividade3-141cc.firebaseapp.com',
+    messagingSenderId: '713147221696',
+  },
   production: false
 };
 
